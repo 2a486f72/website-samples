28103e07bf1cfdf598180d1f2db66eb5df9c42c8
2a <2a@486f72.com>
2013-03-30 15:44:03 +0200

    First draft example for SemaphoreLock
    Shows how to do simple async task mutual exclusion with the lock
    keyword.

---
+++ SemaphoreLock/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SemaphoreLock
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Net.Http;
+	using System.Threading.Tasks;
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			var file = new DownloadableFile();
+
+			using (var stream = File.Create("Output.txt"))
+			{
+				DownloadFileVersion1(file, stream);
+			}
+		}
+
+		private static readonly object _writeLock = new object();
+
+		private static void DownloadFileVersion1(DownloadableFile file, FileStream destination)
+		{
+			var tasks = new List<Task>();
+
+			foreach (int i in Enumerable.Range(0, file.ChunkCount))
+			{
+				int chunkIndex = i;
+
+				tasks.Add(Task.Run(async delegate
+				{
+					var chunkContents = await new HttpClient().GetByteArrayAsync(file.GetChunkUrl(chunkIndex));
+
+					lock (_writeLock)
+						destination.Write(chunkContents, 0, chunkContents.Length);
+				}));
+			}
+
+			Task.WaitAll(tasks.ToArray());
+		}
+
+		private sealed class DownloadableFile
+		{
+			public readonly int ChunkCount = 10;
+
+			public Uri GetChunkUrl(int chunkIndex)
+			{
+				return new Uri("http://google.com");
+			}
+		}
+	}
+}
