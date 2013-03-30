22943e46b0871c273a8d2eef2ddeb3871223b228
2a <2a@486f72.com>
2013-03-30 16:19:34 +0200

    Fourth example - file writes also SemaphoreLocked

--- SemaphoreLock/Program.cs
+++ SemaphoreLock/Program.cs
@@ -17,7 +17,7 @@
 
 			using (var stream = File.Create("Output.txt"))
 			{
-				DownloadFileVersion3(file, stream);
+				DownloadFileVersion4(file, stream);
 			}
 		}
 
@@ -92,6 +92,31 @@
 						chunkContents = await new HttpClient().GetByteArrayAsync(file.GetChunkUrl(chunkIndex));
 
 					lock (_writeLock)
+						destination.Write(chunkContents, 0, chunkContents.Length);
+				}));
+			}
+
+			Task.WaitAll(tasks.ToArray());
+		}
+
+		private static readonly SemaphoreSlim _fileWriteSemaphore = new SemaphoreSlim(1);
+
+		private static void DownloadFileVersion4(DownloadableFile file, FileStream destination)
+		{
+			var tasks = new List<Task>();
+
+			foreach (int i in Enumerable.Range(0, file.ChunkCount))
+			{
+				int chunkIndex = i;
+
+				tasks.Add(Task.Run(async delegate
+				{
+					byte[] chunkContents;
+
+					using (await SemaphoreLock.TakeAsync(_concurrentDownloadSemaphore))
+						chunkContents = await new HttpClient().GetByteArrayAsync(file.GetChunkUrl(chunkIndex));
+
+					using (await SemaphoreLock.TakeAsync(_fileWriteSemaphore))
 						destination.Write(chunkContents, 0, chunkContents.Length);
 				}));
 			}
